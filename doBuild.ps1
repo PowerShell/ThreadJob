7cf46e501e462386ac20500e5efe8d96e8da850f
Paul Higinbotham <paulhi@microsoft.com>
2022-01-24 10:53:51 -0800

    Add net sdk install step (#19)

--- doBuild.ps1
+++ doBuild.ps1
@@ -32,12 +32,14 @@ function DoBuild
         try {
             # Check for dotnet for Windows (we only build on Windows platforms).
             if ($null -eq (Get-Command -Name 'dotnet.exe' -ErrorAction Ignore)) {
+                Write-Verbose -Verbose -Message "dotnet.exe cannot be found in current path."
                 $dotnetCommandPath = Join-Path -Path $env:ProgramFiles -ChildPath "dotnet"
                 $dotnetCommand = Join-Path -Path $dotnetCommandPath -ChildPath "dotnet.exe"
                 if ($null -eq (Get-Command -Name $dotnetCommand -ErrorAction Ignore)) {
                     throw "Dotnet.exe cannot be found: $dotnetCommand is unavailable for build."
                 }
 
+                Write-Verbose -Verbose -Message "Adding dotnet.exe path to PATH variable: $dotnetCommandPath"
                 $env:PATH = $dotnetCommandPath + [IO.Path]::PathSeparator + $env:PATH
             }
 

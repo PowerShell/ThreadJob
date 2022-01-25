f84756211f6b17d18dbc8f9bebbf1d5a2f2da8ec
Paul Higinbotham <paulhi@microsoft.com>
2022-01-25 10:04:24 -0800

    Use full path for dotnet.exe (#20)

--- doBuild.ps1
+++ doBuild.ps1
@@ -30,26 +30,26 @@ function DoBuild
         # build code and place it in the staging location
         Push-Location "${SrcPath}/code"
         try {
-            # Check for dotnet for Windows (we only build on Windows platforms).
-            if ($null -eq (Get-Command -Name 'dotnet.exe' -ErrorAction Ignore)) {
-                Write-Verbose -Verbose -Message "dotnet.exe cannot be found in current path."
-                $dotnetCommandPath = Join-Path -Path $env:ProgramFiles -ChildPath "dotnet"
-                $dotnetCommand = Join-Path -Path $dotnetCommandPath -ChildPath "dotnet.exe"
-                if ($null -eq (Get-Command -Name $dotnetCommand -ErrorAction Ignore)) {
-                    throw "Dotnet.exe cannot be found: $dotnetCommand is unavailable for build."
-                }
+            # Get dotnet.exe command path.
+            $dotnetCommand = Get-Command -Name 'dotnet' -ErrorAction Ignore
 
-                Write-Verbose -Verbose -Message "Adding dotnet.exe path to PATH variable: $dotnetCommandPath"
-                $env:PATH = $dotnetCommandPath + [IO.Path]::PathSeparator + $env:PATH
+            # Check for dotnet for Windows (we only build on Windows platforms).
+            if ($null -eq $dotnetCommand) {
+                Write-Verbose -Verbose -Message "dotnet.exe cannot be found in current path. Looking in ProgramFiles path."
+                $dotnetCommandPath = Join-Path -Path $env:ProgramFiles -ChildPath "dotnet\dotnet.exe"
+                $dotnetCommand = Get-Command -Name $dotnetCommandPath -ErrorAction Ignore
+                if ($null -eq $dotnetCommand) {
+                    throw "Dotnet.exe cannot be found: $dotnetCommandPath is unavailable for build."
+                }
             }
 
             # Check dotnet version
-            Write-Verbose -Verbose -Message "DotNet version: $(dotnet --version)"
+            Write-Verbose -Verbose -Message "DotNet version: $(& ($dotnetCommand) --version)"
 
             # Build source
             Write-Verbose -Verbose -Message "Building with configuration: $BuildConfiguration, framework: $BuildFramework"
             Write-Verbose -Verbose -Message "Building location: PSScriptRoot: $PSScriptRoot, PWD: $pwd"
-            dotnet publish --configuration $BuildConfiguration --framework $BuildFramework --output $BuildSrcPath
+            & ($dotnetCommand) publish --configuration $BuildConfiguration --framework $BuildFramework --output $BuildSrcPath
 
             # Dump build source output directory
             # $outResults = Get-ChildItem -Path "bin/${BuildConfiguration}/${BuildFramework}" -Recurse | Out-String
@@ -71,8 +71,7 @@ function DoBuild
             }
         }
         catch {
-            # Write-Error "dotnet build failed with error: $_"
-            Write-Verbose -Verbose -Message "dotnet build failed with error: $_"
+            Write-Error "dotnet build failed with error: $_"
         }
         finally {
             Pop-Location
